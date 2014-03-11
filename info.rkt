92dbf4540f6ae04e532b3efb1ef966ac1f862f73
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-03-11 18:09:20 -0400

    Add Racket package deps.

--- info.rkt
+++ info.rkt
@@ -1,2 +1,14 @@
 #lang setup/infotab
 (define collection 'multi)
+(define deps '(
+	       ;; these are what the package system tells me I need:
+	       "base"
+	       "net-lib"
+	       "rackunit-lib"
+	       "unstable-contract-lib"
+	       "web-server-lib"
+	       ))
+(define build-deps '(
+		     ;; these are what the package system tells me I need:
+		     "scribble-lib"
+		     ))
